baf90b41ea0cc1a71feab0e039982ffbe6d5577a
Jason Prasad <jasongprasad@gmail.com>
2024-12-15 13:57:47 -0500

    Add initial scanning code

--- src/main/kotlin/com/klox/Lox.kt
+++ src/main/kotlin/com/klox/Lox.kt
@@ -1,9 +1,50 @@
 package com.klox
 
-class Lox {
-    val greeting: String get() = "Hello World!"
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.Scanner
+
+object Lox {
+    private const val USAGE = "Usage: klox [script]"
+
+    @JvmStatic fun main(args: Array<String>) {
+        if (args.size > 1) {
+            println(USAGE)
+        } else if (args.size == 1) {
+            runFile(args.first())
+        } else {
+            runPrompt()
+        }
+    }
+
+    private fun run(source: String) {
+        val scanner = Scanner(source)
+        val tokens = scanner.tokens()
+
+        for (token in tokens) {
+            println(token)
+        }
+    }
+
+    private fun runFile(path: String) {
+        val bytes = Files.readAllBytes(Paths.get(path))
+
+        run(String(bytes, Charset.defaultCharset()))
+    }
+
+    private fun runPrompt() {
+        val input = InputStreamReader(System.`in`)
+        val reader = BufferedReader(input)
+
+        while (true) {
+            print(">  ")
+            val line = reader.readLine()
+
+            line?.let(::run)
+        }
+    }
 }
 
-fun main() {
-    println(Lox().greeting)
-}
